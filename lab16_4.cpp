856a8c0f8ea914e7518c617611fa497b1a59e75c
aeiout <ueaarthon13117@gmail.com>
2023-02-07 17:00:39 +0700

    finished

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,15 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d){
+	int foo[]={a,b,c,d};
+	for(int k=0;k<10;k++){
+		int i=rand()%4;
+		int j=rand()%4;
+		swap(foo[i],foo[j]);
+	}
+		a=foo[0];
+		b=foo[1];
+		c=foo[2];
+		d=foo[3];		
+}
